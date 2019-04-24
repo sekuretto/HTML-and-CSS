c2a4e33d969c3301658774bf133a25283f3a4452
sekuretto <marja.jambeck@gmail.com>
2019-04-24 23:40:29 +0300

    responsive nav

---
+++ responsive_navbar/app.js
@@ -0,0 +1,16 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+    //animate links
+    navLinks.forEach(link, index) => {
+        link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7}s';
+
+    });
+}
+
+navSlide();
